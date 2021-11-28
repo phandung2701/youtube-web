d63756cd43cacfa7e898fe3fbaa223bc9c16961f
NewBie-profesion <ptd012001@gmail.com>
2021-11-28 19:49:48 +0700

    complete the sidebar

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// xu ly phan sidebar
-const sidebar = document.querySelector(".body_sidebar");
-const bodyProduct = document.querySelector(".body__product");
-const categoryHomepage = document.querySelector(".category__homePage");
-const categoryVideo = document.querySelectorAll(".category__video");
-const sidebarCategoryList = categoryHomepage.querySelector(".category__list");
-const categoryItem = sidebarCategoryList.querySelectorAll(
-  ".category__list-item"
-);
-const buttonBar = document.querySelector(".head__logo-bar");
-buttonBar.addEventListener("click", () => {
-  sidebarCategoryList.classList.toggle("small");
-  sidebar.classList.toggle("active");
-  bodyProduct.classList.toggle("active");
-  categoryItem.forEach((x) => x.classList.toggle("small"));
-  categoryVideo.forEach((x) => x.classList.toggle("active"));
-});
-
 // click hint
 const hint = document.querySelectorAll(".product__hint-list-item");
 hint.forEach((x) => {
@@ -24,4 +6,10 @@ hint.forEach((x) => {
     x.classList.add("active");
   });
 });
-console.log(hint);
+// click handle sidebar
+const main = document.querySelector("main");
+const sidebar = document.querySelector(".head__logo-bar");
+sidebar.addEventListener("click", () => {
+  console.log("ok");
+  main.classList.toggle("active");
+});
