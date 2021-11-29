0a4b81e8d97806b0f41174ba4d8d9f52567d96e1
NewBie-profesion <ptd012001@gmail.com>
2021-11-29 09:34:24 +0700

    update dark theme

--- main.js
+++ main.js
@@ -13,3 +13,22 @@ sidebar.addEventListener("click", () => {
   console.log("ok");
   main.classList.toggle("active");
 });
+
+// togle themme
+window.onload = function () {
+  const themeBtn = document.querySelector(".head__user-video");
+  themeBtn.addEventListener("click", function () {
+    // Lấy thuộc tính data-theme
+    const root = document.querySelector(":root");
+    const isLightMode =
+      root.getAttribute("data-theme") === "dark" ? false : true;
+    // toggle theme mode
+    if (isLightMode) {
+      root.setAttribute("data-theme", "dark");
+    } else {
+      root.setAttribute("data-theme", "light");
+    }
+    // thay đổi vị trí của button
+    // this.classList.toggle('active');
+  });
+};
